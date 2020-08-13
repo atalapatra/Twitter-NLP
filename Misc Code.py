0f9323c499020c75e951cdf2cf60272f8931bbf6
Amit Talapatra <amit.talapatra@gmail.com>
2020-08-13 13:53:12 -0400

    2020-08-13

--- Misc Code.py
+++ Misc Code.py
@@ -1,3 +1,45 @@
+## API Rate Limit Status
+
+data = api.rate_limit_status()
+
+data['resources']['statuses']['/statuses/home_timeline']
+data['resources']['users']['/users/lookup']
+
+
+## Sentiment Analysis using TextBlob
+
+# Create a custom search term and define the number of tweets
+search_term = "#cyberpunk 2077 -filter:retweets"
+
+tweets = tweepy.Cursor(api.search,
+                   q=search_term,
+                   lang="en",
+                   since='2020-01-01').items(10)
+
+# Remove URLs
+tweets_no_urls = [Module.remove_url(tweet.text) for tweet in tweets]
+    
+# Create textblob objects of the tweets
+sentiment_objects = [TextBlob(tweet) for tweet in tweets_no_urls]
+
+sentiment_objects[0].polarity, sentiment_objects[0]
+
+sentiment_values = [[tweet.sentiment.polarity, str(tweet)] for tweet in sentiment_objects]
+
+# Create dataframe containing the polarity value and tweet text
+sentiment_df = pd.DataFrame(sentiment_values, columns=["polarity", "tweet"])
+
+# Plot histogram
+fig, ax = plt.subplots(figsize=(8, 6))
+
+# Plot histogram of the polarity values
+sentiment_df.hist(bins=[-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1],
+             ax=ax,
+             color="purple")
+
+plt.show()
+
+
 
 cnn_breaking_news = api.user_timeline('cnnbrk')
 
--- Twitter NLP Code.py
+++ Twitter NLP Code.py
@@ -17,71 +17,36 @@ access_token_secret = connections['Twitter API']['Access Token Secret']
 auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-
-## API Rate Limit Status
-
-data = api.rate_limit_status()
-
-data['resources']['statuses']['/statuses/home_timeline']
-data['resources']['users']['/users/lookup']
-
-
-## Sentiment Analysis using TextBlob
-
-# Create a custom search term and define the number of tweets
-search_term = "#cyberpunk 2077 -filter:retweets"
-
-tweets = tweepy.Cursor(api.search,
-                   q=search_term,
-                   lang="en",
-                   since='2020-01-01').items(10)
-
-# Remove URLs
-tweets_no_urls = [Module.remove_url(tweet.text) for tweet in tweets]
-    
-# Create textblob objects of the tweets
-sentiment_objects = [TextBlob(tweet) for tweet in tweets_no_urls]
-
-sentiment_objects[0].polarity, sentiment_objects[0]
-
-sentiment_values = [[tweet.sentiment.polarity, str(tweet)] for tweet in sentiment_objects]
-
-# Create dataframe containing the polarity value and tweet text
-sentiment_df = pd.DataFrame(sentiment_values, columns=["polarity", "tweet"])
-
-# Plot histogram
-fig, ax = plt.subplots(figsize=(8, 6))
-
-# Plot histogram of the polarity values
-sentiment_df.hist(bins=[-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1],
-             ax=ax,
-             color="purple")
-
-plt.show()
+api = tweepy.API(auth #,
+                 # wait_on_rate_limit=True, 
+                 # wait_on_rate_limit_notify=True
+                 )
 
 
 ## Create list of search terms and combine into dataframe
 
 search_criteria = ['cyberpunk 2077',
-                   'watch dogs legion'#,
-                   # 'hitman 3',
-                   # 'crusader kings 3',
-                   # 'star wars squadrons'
+                   'watch dogs legion',
+                   'hitman 3',
+                   'crusader kings 3',
+                   'star wars squadrons'
                    ]
 
-sentiment_df = pd.DataFrame(columns=["search terms", "polarity", "subjectivity", "tweet"])
+sentiment_df_list = []
 
 for search in search_criteria:
     tweets = tweepy.Cursor(api.search,
                        q=search,
                        lang="en",
-                       since='2020-01-01').items(10)
+                       since='2020-01-01').items(200)
     tweets_no_urls = [Module.remove_url(tweet.text) for tweet in tweets]
     sentiment_objects = [TextBlob(tweet) for tweet in tweets_no_urls]
     sentiment_values = [[str(search), tweet.sentiment.polarity, tweet.sentiment.subjectivity, str(tweet) ] for tweet in sentiment_objects]
     sentiment_values_df = pd.DataFrame(sentiment_values, columns=["search terms", "polarity", "subjectivity", "tweet"])
-    sentiment_df.append(sentiment_values_df)
+    sentiment_df_list.append(sentiment_values_df)
 
-sentiment_df
+sentiment_df = pd.concat(sentiment_df_list)
+
+sentiment_df.hist(column='polarity', by='search terms', figsize = (10,12), range=[-1, 1])
+
+plt.show()
